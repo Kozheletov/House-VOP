4592e49a6e45216328990b6476fd264a65f2a8ab
Grigoriy Kozheletov <misteryrus@gmail.com>
2021-09-13 21:19:57 +0300

    added burger menu for mobile version

--- app/js/dev-js/main.js
+++ app/js/dev-js/main.js
@@ -1,5 +1,21 @@
-const sum = (a, b) => a + b;
+'use strict';
 
-sum(1, 2);
+window.addEventListener('load', () => {
 
-console.log('hello');
+    const burger = document.querySelector('.button-menu');
+    const menu = document.querySelector('.nav');
+    const burgerTopLine = document.querySelector('.button-menu__long-line-top');
+    const burgerMiddleLine = document.querySelector('.button-menu__short-line');
+    const burgerBottomLine = document.querySelector('.button-menu__long-line-bottom');
+
+    burger.addEventListener('click', (event) => {
+        event.preventDefault();
+        menu.classList.toggle('show');
+        burger.classList.toggle('button-menu_close');
+        burgerTopLine.classList.toggle('button-menu__long-line-top_close');
+        burgerMiddleLine.classList.toggle('button-menu__short-line_close');
+        burgerBottomLine.classList.toggle('button-menu__long-line-bottom_close');
+
+    })
+
+});
