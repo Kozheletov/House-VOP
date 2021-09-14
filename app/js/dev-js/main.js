88a56c7f0ec6a723283496a15810cb4f1450b61f
Grigoriy Kozheletov <misteryrus@gmail.com>
2021-09-14 12:45:50 +0300

    fihish header

--- app/js/dev-js/main.js
+++ app/js/dev-js/main.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
 
     const burger = document.querySelector('.button-menu');
     const menu = document.querySelector('.nav');
+    const headerNav = document.querySelector('.header-nav');
+    const headerTitle = document.querySelector('.header-title');
     const burgerTopLine = document.querySelector('.button-menu__long-line-top');
     const burgerMiddleLine = document.querySelector('.button-menu__short-line');
     const burgerBottomLine = document.querySelector('.button-menu__long-line-bottom');
@@ -12,6 +14,9 @@ window.addEventListener('load', () => {
         event.preventDefault();
         menu.classList.toggle('show');
 
+        headerTitle.classList.toggle('visually-hidden');
+        headerTitle.classList.toggle('header-title_menu-open');
+        headerNav.classList.toggle('header-nav_menu-open');
         burger.classList.toggle('button-menu_close');
         burgerTopLine.classList.toggle('button-menu__long-line-top_close');
         burgerMiddleLine.classList.toggle('button-menu__short-line_close');
